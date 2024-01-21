15eec9603c7742a76fb34c59add27abe3016b94a
Alexandr <legendaryneobatman@gmail.com>
2024-01-22 00:51:55 +0500

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,6 @@ import * as directives from 'vuetify/directives'
 import {router} from "@/router";
 import {createPinia} from "pinia";
 import {aliases, mdi} from "vuetify/iconsets/mdi";
-import YmapPlugin from 'vue-yandex-maps'
 import './assets/main.scss'
 const vuetify = createVuetify({
   components,
@@ -27,17 +26,8 @@ const pinia = createPinia()
 
 loadFonts()
 
-const settings = {
-  apiKey: 'e6ac4ad7-3142-4351-9bde-8a86cadb91f2', // Индивидуальный ключ API
-  lang: 'ru_RU', // Используемый язык
-  coordorder: 'latlong', // Порядок задания географических координат
-  debug: false, // Режим отладки
-  version: '2.1' // Версия Я.Карт
-}
-
 createApp(App)
   .use(vuetify)
   .use(router)
   .use(pinia)
-  .use(YmapPlugin, settings)
   .mount('#app')
